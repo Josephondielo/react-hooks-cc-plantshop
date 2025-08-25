7ac012e91760355f8c711f8b73a56b86819ff60a
Josephondielo <josebonagain@gmail.com>
2025-08-25 23:06:45 +0300

    passed

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plant, handleSoldOut }) {
   return (
-    <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+    <li
+      className="card"
+      data-testid="plant-item"
+      style={{ opacity: plant.soldOut ? 0.5 : 1 }}
+    >
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      <button className="primary" onClick={() => handleSoldOut(plant.id)}>
+        {plant.soldOut ? "Out of Stock" : "In Stock"}
+      </button>
     </li>
   );
 }
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,25 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, setPlants }) {
+  const handleSoldOut = (id) => {
+    setPlants(
+      plants.map((plant) =>
+        plant.id === id ? { ...plant, soldOut: !plant.soldOut } : plant
+      )
+    );
+  };
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map((plant) => (
+        <PlantCard
+          key={plant.id}
+          plant={plant}
+          handleSoldOut={handleSoldOut}
+        />
+      ))}
+    </ul>
   );
 }
 
